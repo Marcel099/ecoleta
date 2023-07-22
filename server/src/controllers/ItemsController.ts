436613e84aed977c7e4460be0b55782b2d28ca1f
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-07-22 15:03:24 -0300

    fix(server): Definição de URL da API nas variáveis de ambiente

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -10,7 +10,7 @@ class ItemsController {
             return {
                 id: id,
                 title: title,
-                image_url: `http://192.168.0.102:3333/uploads/${image}`,
+                image_url: `${process.env.API_URL}/uploads/${image}`,
             };
         })
         return response.json(serializedItems)
